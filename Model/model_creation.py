862ee7d7fe47cdf7521d67b7cbdfe2b90082770e
project <project@project.myguest.virtualbox.org>
2023-09-04 11:35:21 +0200

    change for jenkins

--- Model/model_creation.py
+++ Model/model_creation.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-def model_creation(x_train, y_train):
+def model_creation(x_train, y_train, epochs = 10):
     model = Sequential()
     model.add(LSTM(units=32, return_sequences=True, input_shape=(x_train.shape[1], 1)))
     model.add(Dropout(0.2))
@@ -12,7 +12,7 @@ def model_creation(x_train, y_train):
     model.add(Dense(units=32,activation = 'relu'))
     model.add(Dense(units=1))
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=10, batch_size=32)
+    model.fit(x_train, y_train, epochs=epochs, batch_size=32)
     #model.save(name_of_model)
     return model
 
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     days_for_testing = 30
     x_train, y_train, X_test, scaler = model_data_creation(data_to_use, days_for_training, days_for_testing)
 
-    model = model_creation(x_train, y_train)
+    model = model_creation(x_train, y_train, epochs = 3)
