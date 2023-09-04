b4839191c01d350b5b055f4a36a856952184153a
Supersymmetry <supersymmetry@calculator.myguest.virtualbox.org>
2023-09-04 12:12:22 +0200

    change test

--- tests/test_uni_search_input.py
+++ tests/test_uni_search_input.py
@@ -7,7 +7,7 @@ class TestSearchInput(unittest.TestCase):
 
     def test_valid_stock_symbol_default_period(self):
         stock_symbol = 'ALO.PA'
-        data, data_to_use = search_input(stock_symbol)
+        data, data_to_use = search_input(stock_symbol, jenkins = False)
         self.assertIsInstance(data, pd.DataFrame)
         self.assertIsInstance(data_to_use, np.ndarray)
         self.assertEqual(data_to_use.shape[0], len(data))
@@ -16,7 +16,7 @@ class TestSearchInput(unittest.TestCase):
     def test_valid_stock_symbol_custom_period(self):
         stock_symbol = 'ALO.PA'
         period = '2y'
-        data, data_to_use = search_input(stock_symbol, period)
+        data, data_to_use = search_input(stock_symbol, period, jenkins = False)
         self.assertIsInstance(data, pd.DataFrame)
         self.assertIsInstance(data_to_use, np.ndarray)
         self.assertEqual(data_to_use.shape[0], len(data))
@@ -24,27 +24,27 @@ class TestSearchInput(unittest.TestCase):
 
     def test_invalid_stock_symbol(self):
         stock_symbol = 'INVALID'
-        data, data_to_use = search_input(stock_symbol)
+        data, data_to_use = search_input(stock_symbol, jenkins = False)
         self.assertIsNone(data)
         self.assertIsNone(data_to_use)
 
     def test_invalid_period_format(self):
         stock_symbol = 'AAPL'
         period = 'abc'
-        data, data_to_use = search_input(stock_symbol, period)
+        data, data_to_use = search_input(stock_symbol, period, jenkins = False)
         self.assertIsNone(data)
         self.assertIsNone(data_to_use)
 
     def test_no_data_for_period(self):
         stock_symbol = 'AAPL'
         period = '50y'
-        data, data_to_use = search_input(stock_symbol, period)
+        data, data_to_use = search_input(stock_symbol, period, jenkins = False)
         self.assertIsNone(data)
         self.assertIsNone(data_to_use)
 
     def test_consistency_data_and_data_to_use(self):
         stock_symbol = 'GOOGL'
-        data, data_to_use = search_input(stock_symbol)
+        data, data_to_use = search_input(stock_symbol, jenkins = False)
         self.assertEqual(len(data), data_to_use.shape[0])
         self.assertEqual(len(data_to_use.shape), 1)
 
